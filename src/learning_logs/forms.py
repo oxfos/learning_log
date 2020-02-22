1e48ec63d05502d412b73fc034a4a8c0c47a38dc
oxfos <rinaldo_bertossa@hotmail.com>
2020-02-22 14:18:15 +0100

    Added form to input entry.

--- src/learning_logs/forms.py
+++ src/learning_logs/forms.py
@@ -1,5 +1,5 @@
 from django import forms
-from .models import Topic
+from .models import Topic, Entry
 
 class TopicForm(forms.ModelForm):
     """Form to collect/display data about a specific topic."""
@@ -7,3 +7,11 @@ class TopicForm(forms.ModelForm):
         model = Topic
         fields = ['text']
         labels = {'text': ''}
+
+class EntryForm(forms.ModelForm):
+    """Form to register an entry."""
+    class Meta:
+        model = Entry
+        fields = ['text']
+        labels = {'text': 'Entry:'}
+        widgets = {'text': forms.Textarea(attrs={'cols': 80})}
