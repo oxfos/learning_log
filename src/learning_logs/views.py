263653e05f1ae7e568052f8b8b94f95c93a6c44d
oxfos <rinaldo_bertossa@hotmail.com>
2020-02-22 16:07:01 +0100

    Added code to edit entry: url, view, template, link from other pages.

--- src/learning_logs/views.py
+++ src/learning_logs/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render, redirect
-from .models import Topic
+from .models import Topic, Entry
 from .forms import TopicForm, EntryForm
 
 def index(request):
@@ -49,3 +49,19 @@ def new_entry(request, topic_id):
             return redirect('learning_logs:topic', topic_id=topic_id)
     context = {'form': form, 'topic': topic}
     return render(request, 'learning_logs/new_entry.html', context)
+
+def edit_entry(request, entry_id):
+    """Edit an entry."""
+    entry = Entry.objects.get(id=entry_id)
+    topic = entry.topic
+    if request.method != 'POST':
+        # Initial request; pre-fill form with the current entry.
+        form = EntryForm(instance=entry)
+    else:
+        # POST data submitted; process data.
+        form = EntryForm(instance=entry, data=request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('learning_logs:topic', topic_id=topic.id)
+    context = {'form': form, 'topic': topic, 'entry': entry}
+    return render(request, 'learning_logs/edit_entry.html', context)
