63493d33dc6fbc455485a3500fcfa689a10f3612
oxfos <rinaldo_bertossa@hotmail.com>
2020-02-23 12:39:45 +0100

    Refactored code in learning_logs.views: function check_topic_onwer checks topic.owner == request.user.

--- src/learning_logs/views.py
+++ src/learning_logs/views.py
@@ -20,8 +20,7 @@ def topic(request, topic_id):
     """Show a single topic and all its entries."""
     topic = Topic.objects.get(id=topic_id)
     # Make sure the topic belongs to the current user.
-    if topic.owner != request.user:
-        raise Http404
+    check_topic_owner(topic, request)
     entries = topic.entry_set.order_by('-date_added')
     context = {'topic': topic, 'entries': entries}
     return render(request, 'learning_logs/topic.html', context)
@@ -48,6 +47,7 @@ def new_topic(request):
 def new_entry(request, topic_id):
     """Add a new entry for a particular topic."""
     topic = Topic.objects.get(id=topic_id)
+    check_topic_owner(topic, request)
     if request.method != 'POST':
         # No data submitted; create a blank form.
         form = EntryForm()
@@ -66,8 +66,7 @@ def edit_entry(request, entry_id):
     """Edit an entry."""
     entry = Entry.objects.get(id=entry_id)
     topic = entry.topic
-    if topic.owner != request.user:
-        raise Http404
+    check_topic_owner(topic, request)
     if request.method != 'POST':
         # Initial request; pre-fill form with the current entry.
         form = EntryForm(instance=entry)
@@ -79,3 +78,8 @@ def edit_entry(request, entry_id):
             return redirect('learning_logs:topic', topic_id=topic.id)
     context = {'form': form, 'topic': topic, 'entry': entry}
     return render(request, 'learning_logs/edit_entry.html', context)
+
+def check_topic_owner(topic, request):
+    """Checking if logged-in user is topic owner."""
+    if topic.owner != request.user:
+        raise Http404
