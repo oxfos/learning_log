cad2b40ea71bd54942fd4b2464e478e79f17c5be
oxfos <rinaldo_bertossa@hotmail.com>
2020-02-24 22:04:05 +0100

    Modified settings.py.

--- src/learning_log/settings.py
+++ src/learning_log/settings.py
@@ -10,6 +10,8 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.0/ref/settings/
 """
 
+
+import django_heroku
 import os
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
@@ -132,5 +134,4 @@ STATIC_URL = '/static/'
 LOGIN_URL = 'users:login'
 
 # Heroku settings.
-import django_heroku
 django_heroku.settings(locals())
